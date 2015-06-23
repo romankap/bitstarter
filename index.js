2a35bbe13eb6779a9f52d59a42b6852c97cd3ad9
romankap <romankap@gmail.com>
2015-06-23 15:52:17 +0300

    Added MNIST batches
    Downloaded the ones used by Karpathy

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express')
 var app = express();
+var http = require('http');
+var path = require('path');
 var fs = require('fs')
 
+/*
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
@@ -14,3 +17,43 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+*/
+http.createServer(function (request, response) {
+    console.log('request starting...');
+	
+	var filePath = '.' + request.url;
+	if (filePath == './')
+		filePath = './index.htm';
+		
+	var extname = path.extname(filePath);
+	var contentType = 'text/html';
+	switch (extname) {
+		case '.js':
+			contentType = 'text/javascript';
+			break;
+		case '.css':
+			contentType = 'text/css';
+			break;
+	}
+	
+	path.exists(filePath, function(exists) {
+	
+		if (exists) {
+			fs.readFile(filePath, function(error, content) {
+				if (error) {
+					response.writeHead(500);
+					response.end();
+				}
+				else {
+					response.writeHead(200, { 'Content-Type': contentType });
+					response.end(content, 'utf-8');
+				}
+			});
+		}
+		else {
+			response.writeHead(404);
+			response.end();
+		}
+	});	
+}).listen(8080);
+console.log('Server running at http://127.0.0.1:8080/');
