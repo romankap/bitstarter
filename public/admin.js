8357e265c9366bb4d54a998a8b7f77dba1b972e2
romankap <romankap@gmail.com>
2015-07-18 13:30:43 +0300

    Admin: fixed the 'toggle_validation' button

--- public/admin.js
+++ public/admin.js
@@ -1,6 +1,6 @@
 var layer_defs, net, trainer;
 var old_net, curr_net;
-var curr_model_ID;
+var curr_model_ID, curr_batch_num;
 
 // ------------------------
 // BEGIN CIFAR10 SPECIFIC STUFF
@@ -64,9 +64,11 @@ var get_batch_num_from_server = function() {
     var parameters = {model_name: "CIFAR10" };
     $.get('/get_batch_num_from_server', parameters, function(data) {
         console.log("<get_batch_num_from_server> Starting to work on batch_num: " + data.batch_num);
+        curr_batch_num = data.batch_num;
         return data.batch_num;
     });
 }
+/*
 var get_net_and_batch_from_server = function() {
     var parameters = {model_name: "CIFAR10"};
     var batch_num;
@@ -91,17 +93,26 @@ var get_net_and_batch_from_server = function() {
         update_net_param_display();
     });
     return batch_num;
-}
+}*/
 
 var get_net_and_batch_from_server = function() {
     var parameters = {model_name: "CIFAR10" };
     $.get('/get_net_and_batch_from_server', parameters, function(data) {
         curr_model_ID  = data.model_ID;
-        console.log("<get_net_and_batch_from_server> Received "+ data.model_name + " model with model_ID: " + curr_model_ID);
+        console.log("<get_net_and_batch_from_server> Received "+ parameters.model_name + " model with model_ID: " + curr_model_ID);
+
         var net_in_Json = JSON.parse(data.net);
         net = new convnetjs.Net();
         net.fromJSON(net_in_Json);
         reset_all();
+
+        curr_batch_num = data.batch_num;
+        update_displayed_batch_num(curr_batch_num);
+
+        var vis_elt = document.getElementById("visnet");
+        visualize_activations(net, vis_elt);
+        test_predict();
+        update_net_param_display();
     });
 }
 
